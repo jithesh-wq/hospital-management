a8e26b0b46120d37b09e11182a06ef65453e4485
jithesh-wq <jitheshtv4@gmail.com>
2021-07-17 13:03:37 +0530

    adding notificationcontainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import Count from './components/Count';
-import Doctor from './components/Doctor';
-import Notification from './components/Notification';
+import NavItem from './components/NavItem';
+import DashBoard from './pages/DashBoard'
 import {
   Route,
   NavLink,
@@ -10,10 +9,58 @@ import {
 function App() {
   return (
     <HashRouter>
-      <div className="App">
-        <Doctor/>
-        <Count/>
-        <Notification/>
+      <div className="appContainer">
+        <div className="navContainer">
+             <div className="logo">
+                  <img className="logoImg" src="https://i.pinimg.com/originals/46/0b/ec/460bec2777fe7d4a4fec0dde54fdabde.png"/>
+             </div>
+             <ul className="navLinks">
+                <li>
+                  <NavLink className="link" style={{textDecoration:'none',}} to="/">
+                    <NavItem linkName="dashboard"/>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="link" style={{textDecoration:'none'}} to="/covidCases">
+                    <NavItem linkName="covid Cases"/>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="link" style={{textDecoration:'none'}} to="/allpatients">
+                    <NavItem linkName="all patients"/>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="link" style={{textDecoration:'none',}} to="/admitpatient">
+                    <NavItem linkName="admit"/>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="link" style={{textDecoration:'none',}} to="/dischargepatient">
+                    <NavItem linkName="discharge"/>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="link" style={{textDecoration:'none',}} to="/updatepatient">
+                    <NavItem linkName="update"/>
+                  </NavLink>
+                </li>
+            </ul>
+            <div className="bottomContainer">
+              <ul className="navLinks">
+                <li>
+                  <NavLink className="link" style={{textDecoration:'none',}} to="/updatepatient">
+                        <NavItem linkName="logout"/>
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+        </div>
+        <div className = "pageContainer">
+            <Route exact path="/" component={DashBoard}/>
+            {/* <Route path="/stuff" component={Stuff}/>
+            <Route path="/contact" component={Contact}/> */}
+        </div>
       </div>
     </HashRouter>
   );
--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import './DashBoard.css'
+import Notification from '../components/Notification'
 function DashBoard() {
     return (
-        <div>
-            
+        <div className = "dashboardContainer">
+            <div className = "firstContainer"></div>
+            <div className = "secondContainer">
+                <div className = "allNotificationContainer">
+                        <p className="containerTitle">Notifications</p>
+                        <div className = "notificationList">
+                            <Notification/>
+                            <Notification/>
+                            <Notification/>
+                            <Notification/>
+                            <Notification/>
+                            <Notification/>
+                            <Notification/>
+                            <Notification/>
+                        </div>
+
+
+                </div>
+            </div>
+            <div className = "thirsContainer"></div>
         </div>
     )
 }
